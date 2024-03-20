e33c2c01f8117d05c1607ce98573b8de9ae0c202
md azzizul islam <127038709+Azizul-11@users.noreply.github.com>
2024-03-20 20:49:09 +0530

    Add files via upload

---
+++ ConversionOperation4.js
@@ -0,0 +1,40 @@
+// ****************** Operations ****************
+
+let value=3;
+let negativeValue= -value;
+
+// console.log(negativeValue);
+
+// 1. Arithmetic Operators
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+
+
+
+
+// let str1="Hello"
+// let str2=" Ramiz";
+
+// let str3=str1+str2;
+// console.log(str3);
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+
+//assignment operatora
+
+
+let gameCounter=0;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
