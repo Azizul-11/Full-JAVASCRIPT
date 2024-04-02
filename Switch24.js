a46872c2570114e71f0d37882f30a4fb2796542b
md azzizul islam <127038709+Azizul-11@users.noreply.github.com>
2024-04-02 20:37:08 +0530

    Add files via upload

---
+++ Switch24.js
@@ -0,0 +1,41 @@
+// syntax of Switch Case
+
+// switch (key) { kye means which value i need to check
+
+//     case value:  value you want to check
+        
+//         break; // break is important in switch case 
+                 // if we don't put break in switch case then it will 
+                 // execute other conditions 
+
+//     default: // default value
+//         break;
+// }
+
+
+const month= 3;
+
+switch (month) {
+    case 1:
+        console.log("January"); 
+        break;
+    case 2:
+        console.log("February"); 
+        break;
+    case 3:
+        console.log("March"); 
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case Match"); 
+        break;
+}
+
+// March
+
+
+
+
