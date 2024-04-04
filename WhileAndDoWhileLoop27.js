8d51a0e0f1b499a5918685e9f0aa59cc935a424c
md azzizul islam <127038709+Azizul-11@users.noreply.github.com>
2024-04-05 00:38:09 +0530

    Add files via upload

---
+++ WhileAndDoWhileLoop27.js
@@ -0,0 +1,54 @@
+// while Loop
+
+// while (condition) {
+    
+// }
+
+let index=0
+
+while (index<=10) {
+    console.log(`Value of index is ${index}`);
+    index++
+}
+
+
+
+let myArray= ["Flash", "Batman", "superman", ];
+
+
+let arr=0
+
+while(arr<myArray.length)
+{
+    console.log(`SuperHero's name ${myArray[arr]}`);
+    arr++
+}
+
+
+// do-while loop
+
+// do {
+    
+// } while (condition);
+
+
+
+
+let score=10
+
+do {
+    console.log(`Score: ${score}`);
+    score++
+} while (score <= 10);
+
+
+// do-while loop atleast runs on time 
+
+let Gamescore=11
+
+do {
+    console.log(`Score: ${Gamescore}`);
+    Gamescore++
+} while (Gamescore <= 10);
+
+
