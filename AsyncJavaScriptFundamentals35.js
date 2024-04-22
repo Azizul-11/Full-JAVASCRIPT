fbfba9fc28e57626951c1750210bf1670e9adbe4
md azzizul islam <127038709+Azizul-11@users.noreply.github.com>
2024-04-22 15:45:02 +0530

    Add files via upload

---
+++ AsyncJavaScriptFundamentals35.js
@@ -0,0 +1,23 @@
+// Theory
+
+// Async Code
+
+// javascript -> synchronous and Single Threaded Language
+
+// Execution Context -> execute one line of code at a time
+
+// each operation waits for the last one to complete before executing
+
+// Blocking Code Vs Non Blocking Code
+
+// Blocking code -> Block the flow of Program -> Read File Sync
+
+// Non Blocking code -> Does not Block execution -> Read File Async
+
+// js Engine made with -> memory heap And CallStack
+
+
+// call stack -> function call -> setTimeout -> register callback ->
+// task Queue-> add To CallStack -> Global
+
+
